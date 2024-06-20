505335f6812219b3b6ab6b0e98b26454e7b354bf
sandy09yadav <sonu09yadav03@example.com>
2024-06-20 10:12:23 +0530

    navbar item

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -5,14 +5,18 @@ const Dashboard = () => {
 
     const [isActive, setIsActive] = useState(0);
 
+    const handleClick = (index) => {
+        setIsActive(index)
+    }
+
     const tabs = ["Dashboard", "User details", "CMS", "Orders/Activity", "Plans/Events", "Support", "Change Pass.", "Admin Manage", "LOGOUT"]
     return (
         <>
             <navbar className=' block flex' style={{ backgroundColor: '#242424' }}>
                 <ul className='bg-black w-2/12 h-min rounded-br-lg rounded-bl-2xl'>
-                    {tabs.map((data) => {
+                    {tabs.map((data, index) => {
                         return (
-                            <li className='text-white py-4 px-10 '>
+                            <li key={index} className={`text-white py-4 px-10 cursor-pointer ${isActive === index ? 'bg-gray-700' : ''}`} onClick={() => handleClick(index)}>
                                 <a href='#'>
                                     {data}
                                 </a>
