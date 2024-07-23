39ac77aeaa45e9abf8d8815f105c3875215fe489
jayaskanhaiya0001 <jayaskanhaiya0001@gmail.com>
2024-07-23 21:46:19 +0530

    Driver Management

--- src/dashboard/listItems.js
+++ src/dashboard/listItems.js
@@ -10,58 +10,127 @@ import BarChartIcon from '@mui/icons-material/BarChart';
 import LayersIcon from '@mui/icons-material/Layers';
 import AssignmentIcon from '@mui/icons-material/Assignment';
 import { FaUserCircle } from "react-icons/fa";
-import { NavLink } from 'react-router-dom';
-export const mainListItems = (
-  <React.Fragment>
-    <NavLink to={'/dashboard'}>
+import { NavLink, useLocation } from 'react-router-dom';
 
-      <ListItemButton>
-        <ListItemIcon>
-          <DashboardIcon />
-        </ListItemIcon>
-        <ListItemText primary="Dashboard" />
-      </ListItemButton>
-    </NavLink>
-    {/* <ListItemButton>
-      <ListItemIcon>
-        <FaUserCircle />
-      </ListItemIcon>
-      <ListItemText primary="User details" />
-    </ListItemButton> */}
-    <NavLink to={'/rides'}>
-      <ListItemButton>
-        <ListItemIcon>
-          <PeopleIcon />
-        </ListItemIcon>
-        <ListItemText primary="Ride Management" />
-      </ListItemButton>
-    </NavLink>
-    <NavLink to={'/drivers'}>
-      <ListItemButton>
-        <ListItemIcon>
-          <BarChartIcon />
-        </ListItemIcon>
-        <ListItemText primary="Drivers" />
-      </ListItemButton>
-    </NavLink>
-    <NavLink to={'/packages-detail'}>
-      <ListItemButton>
-        <ListItemIcon>
-          <BarChartIcon />
-        </ListItemIcon>
-        <ListItemText primary="Packages Detail" />
-      </ListItemButton>
-    </NavLink>
-    <NavLink to={'/co-admin'}>
-      <ListItemButton>
-        <ListItemIcon>
-          <LayersIcon />
-        </ListItemIcon>
-        <ListItemText primary="Co-Admins" />
-      </ListItemButton>
-    </NavLink>
-  </React.Fragment>
-);
+export const MainListItems = () => {
+  const location = useLocation()
+  return (
+
+    <React.Fragment>
+      <NavLink to={'/dashboard'}>
+
+        <ListItemButton sx={{
+          backgroundColor: location.pathname === '/dashboard' ? '#DD781E' : 'inherit',
+          '&:hover': {
+            backgroundColor: '#DD781E',
+            color: "#fff"
+          },
+          '&.Mui-selected': {
+            backgroundColor: '#DD781E',
+            color: "#fff",
+            '&:hover': {
+              backgroundColor: '#DD781E',
+              color: "#fff"
+            },
+          },
+        }}>
+          <ListItemIcon>
+            <DashboardIcon />
+          </ListItemIcon>
+          <ListItemText primary="Dashboard" />
+        </ListItemButton>
+      </NavLink>
+      <NavLink to={'/rides'}>
+        <ListItemButton sx={{
+          backgroundColor: location.pathname === '/rides' ? '#DD781E' : 'inherit',
+          '&:hover': {
+            backgroundColor: '#DD781E',
+            color: "#fff"
+          },
+          '&.Mui-selected': {
+            backgroundColor: '#DD781E',
+            color: "#fff",
+            '&:hover': {
+              backgroundColor: '#DD781E',
+              color: "#fff"
+            },
+          },
+        }}>
+          <ListItemIcon>
+            <PeopleIcon />
+          </ListItemIcon>
+          <ListItemText primary="Ride Management" />
+        </ListItemButton>
+      </NavLink>
+      <NavLink to={'/drivers'}>
+        <ListItemButton sx={{
+          backgroundColor: location.pathname === '/drivers' ? '#DD781E' : 'inherit',
+          '&:hover': {
+            backgroundColor: '#DD781E',
+            color: "#fff"
+          },
+          '&.Mui-selected': {
+            backgroundColor: '#DD781E',
+            color: "#fff",
+            '&:hover': {
+              backgroundColor: '#DD781E',
+              color: "#fff"
+            },
+          },
+        }}>
+          <ListItemIcon>
+            <BarChartIcon />
+          </ListItemIcon>
+          <ListItemText primary="Drivers" />
+        </ListItemButton>
+      </NavLink>
+      <NavLink to={'/packages-detail'}>
+        <ListItemButton sx={{
+          backgroundColor: location.pathname === '/packages-detail' ? '#DD781E' : 'inherit',
+          '&:hover': {
+            backgroundColor: '#DD781E',
+            color: "#fff"
+          },
+          '&.Mui-selected': {
+            backgroundColor: '#DD781E',
+            color: "#fff",
+            '&:hover': {
+              backgroundColor: '#DD781E',
+              color: "#fff"
+            },
+          },
+        }}>
+          <ListItemIcon>
+            <BarChartIcon />
+          </ListItemIcon>
+          <ListItemText primary="Packages Detail" />
+        </ListItemButton>
+      </NavLink>
+      <NavLink to={'/co-admin'}>
+        <ListItemButton sx={{
+        backgroundColor: location.pathname === '/co-admin' ? '#DD781E' : 'inherit',
+        '&:hover': {
+          backgroundColor: '#DD781E',
+          color: "#fff"
+        },
+        '&.Mui-selected': {
+          backgroundColor: '#DD781E',
+          color: "#fff",
+          '&:hover': {
+            backgroundColor: '#DD781E',
+            color: "#fff"
+          },
+        },
+      }}>
+          <ListItemIcon>
+            <LayersIcon />
+          </ListItemIcon>
+          <ListItemText primary="Co-Admins" />
+        </ListItemButton>
+      </NavLink>
+    </React.Fragment>
+  )
+};
 
 export const secondaryListItems = (
   <>
