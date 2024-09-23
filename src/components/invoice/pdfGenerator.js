a906c240691c5a67c45cabea2518858f04cc2dca
jayaskanhaiya0001 <jayaskanhaiya0001@gmail.com>
2024-09-23 23:27:59 +0530

    Invoice

--- src/components/invoice/pdfGenerator.js
+++ src/components/invoice/pdfGenerator.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
-
+import { Box, Grid, Typography } from '@mui/material';
+import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+const pdfStyles = StyleSheet.create({
+  page: {
+    padding: 20,
+    fontSize: 10,
+  },
+  header: {
+    marginBottom: 20,
+  },
+  table: {
+    display: 'table',
+    width: 'auto',
+    marginBottom: 20,
+  },
+  tableRow: {
+    flexDirection: 'row',
+  },
+  tableCell: {
+    padding: 5,
+    borderBottom: '1pt solid black',
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  footer: {
+    marginTop: 30,
+    textAlign: 'right',
+  },
+});
 export const generatePDF = (data, ref) => {
   const input = ref.current; // Get the DOM element for the invoice
   html2canvas(input, { scale: 1 })
@@ -21,7 +50,8 @@ export const generatePDF = (data, ref) => {
 
 const Invoice = React.forwardRef(({ data }, ref) => {
   return (
-    <div
+    <>
+      {/* <div
       ref={ref}
       style={{
         width: '210mm', // A4 width
@@ -43,7 +73,81 @@ const Invoice = React.forwardRef(({ data }, ref) => {
           ))}
         </tbody>
       </table>
-    </div>
+    </div> */}
+      <Document>
+        <Page size="A4" style={pdfStyles.page}>
+          {/* Header Section */}
+          <View style={pdfStyles.header}>
+            <Text style={pdfStyles.bold}>DRIVEASSIST TECHNOLOGIES INDIA PRIVATE LIMITED</Text>
+            <Text>Address Line 1, City, Postal Code</Text>
+            <Text>GSTIN: 07AAxxxxEXXXZZ</Text>
+          </View>
+
+          {/* Bill To Section */}
+          <View>
+            <Text style={pdfStyles.bold}>Bill To:</Text>
+            <Text>BGGS SOLUTIONS PRIVATE LIMITED</Text>
+            <Text>Address Line 1, City, Postal Code</Text>
+            <Text>GSTIN: 07AAxxxxEXXXZZ</Text>
+          </View>
+
+          {/* Invoice Information */}
+          <View>
+            <Text>Invoice No: INV305</Text>
+            <Text>Date: 23-09-2024</Text>
+            <Text>Due Date: 23-09-2024</Text>
+          </View>
+
+          {/* Table */}
+          <View style={pdfStyles.table}>
+            {/* Table Header */}
+            <View style={pdfStyles.tableRow}>
+              <Text style={[pdfStyles.tableCell, pdfStyles.bold]}>S.No</Text>
+              <Text style={[pdfStyles.tableCell, pdfStyles.bold]}>Item Description</Text>
+              <Text style={[pdfStyles.tableCell, pdfStyles.bold]}>HSN</Text>
+              <Text style={[pdfStyles.tableCell, pdfStyles.bold]}>Qty</Text>
+              <Text style={[pdfStyles.tableCell, pdfStyles.bold]}>Rate</Text>
+              <Text style={[pdfStyles.tableCell, pdfStyles.bold]}>Taxable Value</Text>
+              <Text style={[pdfStyles.tableCell, pdfStyles.bold]}>GST</Text>
+              <Text style={[pdfStyles.tableCell, pdfStyles.bold]}>Amount</Text>
+            </View>
+
+            {/* Table Rows */}
+            <View style={pdfStyles.tableRow}>
+              <Text style={pdfStyles.tableCell}>1</Text>
+              <Text style={pdfStyles.tableCell}>Driver Charges</Text>
+              <Text style={pdfStyles.tableCell}>998515</Text>
+              <Text style={pdfStyles.tableCell}>1</Text>
+              <Text style={pdfStyles.tableCell}>1,400</Text>
+              <Text style={pdfStyles.tableCell}>1,400.00</Text>
+              <Text style={pdfStyles.tableCell}>70</Text>
+              <Text style={pdfStyles.tableCell}>1,470.00</Text>
+            </View>
+            <View style={pdfStyles.tableRow}>
+              <Text style={pdfStyles.tableCell}>2</Text>
+              <Text style={pdfStyles.tableCell}>Driver Night Charges</Text>
+              <Text style={pdfStyles.tableCell}>996423</Text>
+              <Text style={pdfStyles.tableCell}>1</Text>
+              <Text style={pdfStyles.tableCell}>200</Text>
+              <Text style={pdfStyles.tableCell}>200.00</Text>
+              <Text style={pdfStyles.tableCell}>10</Text>
+              <Text style={pdfStyles.tableCell}>210.00</Text>
+            </View>
+          </View>
+
+          {/* Payment and Total Section */}
+          <View>
+            <Text>Total Amount: ₹1,680.00</Text>
+            <Text>Amount in Words: One Thousand Six Hundred and Eighty Only</Text>
+          </View>
+
+          {/* Footer */}
+          <View style={pdfStyles.footer}>
+            <Text>Authorized Signatory</Text>
+          </View>
+        </Page>
+      </Document>
+    </>
   );
 });
 
